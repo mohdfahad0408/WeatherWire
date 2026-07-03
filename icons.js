90bd46be6ff9f2b2624e54fc052376f56ad2f1c7
Arpita Awasthi <awasthi.arpita11@gmail.com>
2026-07-03 22:56:30 +0530

    Add remove icon SVG to icons.js

--- icons.js
+++ icons.js
@@ -105,6 +105,12 @@ export const ICONS = {
       <path d="M12 5v14"/>
     </svg>
   `,
+  remove: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x">
+    <path d="M18 6 6 18"/>
+    <path d="m6 6 12 12"/>
+    </svg>
+  `,
   "map-pin": `
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin">
       <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/>
